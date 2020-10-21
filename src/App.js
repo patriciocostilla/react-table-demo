553994af01ca445f10e3581768de43d7f9d67e54
Patricio Costilla <patriciocostilla@gmail.com>
2020-10-21 22:30:20 +0000

    adding some functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import "./App.css";
 import { MyTable } from "./components";
 
 function App() {
+  let [columns, setColumns] = React.useState([]);
+  let [data, setData] = React.useState([]);
+  const addColumn = () => setColumns([...columns, {Header: `Column ${columns.length + 1}`, accessor: `col${columns.length + 1}`}])
+  const addRow = () => setData([...data, {col1: `R${data.length + 1}`}]);
   return (
     <div className="App">
-      <MyTable></MyTable>
+      <button onClick={addColumn}>Add column</button>
+      <button onClick={addRow}>Add row</button>
+      <MyTable columns={columns} data={data}/>
     </div>
-  );
+  ); 
 }
 
 export default App;
